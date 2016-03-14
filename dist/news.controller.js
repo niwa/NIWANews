f81900f95f2325070b0cea9f596fa04c88ae9698
uwe duesing <uwe.duesing@gmail.com>
2016-03-14 13:36:15 +1300

    Fixed model

--- dist/news.controller.js
+++ dist/news.controller.js
@@ -1,17 +1,19 @@
 news.controller('newsController', ['$scope', 'newsService', function ($scope, newsService) {
 
-    $scope.model = {
-        headlineIds: [],
-        nodeListRaw: [],
-        nodeListSorted: []
+    $scope.news={
+        model: {
+            headlineIds: [],
+            nodeListRaw: [],
+            nodeListSorted: []
+        }
     };
 
 
-    $scope.$watch('model.headlineIds', function (ids, oldids) {
+    $scope.$watch('news.model.headlineIds', function (ids, oldids) {
 
         if ((typeof ids != 'undefined')&&(ids.length !== 0)) {
             newsService.getNodes(ids).then(function (nodes) {
-                $scope.model.nodeListSorted = nodes;
+                $scope.news.model.nodeListSorted = nodes;
             });
         }
     });
@@ -19,10 +21,10 @@ news.controller('newsController', ['$scope', 'newsService', function ($scope, ne
     $scope.getLatestNodes = function () {
 
         newsService.getLatestNodeIds().then(function (ids) {
-            $scope.model.headlineIds = ids;
+            $scope.news.model.headlineIds = ids;
 
         },function(reason) {
-            $scope.model.nodeListSorted = false;
+            $scope.news.model.nodeListSorted = false;
         });
 
     };
--- dist/news.directive.js
+++ dist/news.directive.js
@@ -9,7 +9,7 @@ news.directive('latestNews', [function () {
         '<div class="panel-heading">' +
         '<h1 class="panel-title">Latest NIWA News </h1>' +
         '</div>' +
-        '<div ng-repeat="node in model.nodeListSorted" class="body">' +
+        '<div ng-repeat="node in news.model.nodeListSorted" class="body">' +
         '<img width="100%" ng-show="$index==0" ng-src="{{node.nodeImage}}" />' +
         '<h3>{{node.title}}</h3>' +
         '<p>{{node.summary}}</p>' +
